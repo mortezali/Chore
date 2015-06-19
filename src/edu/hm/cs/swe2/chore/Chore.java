240a4b9a4fe794bc0e0612f73e3b921196d8830c
Paul Seer <pseer@hm.edu>
2015-06-19 15:41:33 +0200

    corrected Core class

--- src/edu/hm/cs/swe2/chore/Chore.java
+++ src/edu/hm/cs/swe2/chore/Chore.java
@@ -5,19 +5,27 @@ public class Chore {
 	private final String[] chore = { "Klo Putzen", "KÃ¼che wischen", "AbspÃ¼len",
 			"Kochen" };
 
-	public String[] getChore(Chore index) {
-		return chore;
+	
+	//nochmal überprüfen Morteza
+	String getChore(int index) {
+		return chore[index];
 	}
 
-	public void printChore(Chore index) {
-		for(int i = 0; i < chore.length; i++)
-		{
-		
-		System.out.println("Name " + i + ": " + chore[i]); 
-		}
+//	public void printChore(Chore index) {
+//		for(int i = 0; i < chore.length; i++)
+//		{
+//		
+//		System.out.println("Name " + i + ": " + chore[i]); 
+//		}
+//	}
+	
+	//nochmal 1) c genau durchlesen
+	void printChore(int index){
+		System.out.println(getChore(index));
 	}
 	
-	public int getNumberOfChores(){
+	
+	int getNumberOfChores(){
 		
 		return chore.length;
 	}
